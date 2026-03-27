a96080d1c576921a1596c8b34da15ada4ab7c1bc
小白 <78636314+xiaobai5211314@users.noreply.github.com>
2026-03-28 01:26:26 +0800

    更新 FundData.cs

--- Models/FundData.cs
+++ Models/FundData.cs
@@ -7,5 +7,9 @@ namespace 估值助手.Models
         public string FundName { get; set; } = string.Empty;
         public double EstimatedRate { get; set; } // 实时估值涨跌幅
         public DateTime FetchTime { get; set; }   // 抓取时间
+        // 👇 新增：雷达探针专属储物间
+        public double ActualRate { get; set; }
+        public double DiffRate { get; set; }
+
     }
 }
