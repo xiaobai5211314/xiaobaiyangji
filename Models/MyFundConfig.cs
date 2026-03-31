eca4e84b9f5be539f144161236127eb208a46e31
小白 <78636314+xiaobai5211314@users.noreply.github.com>
2026-03-31 12:23:16 +0800

    更新 MyFundConfig.cs

--- Models/MyFundConfig.cs
+++ Models/MyFundConfig.cs
@@ -6,7 +6,9 @@ namespace 估值助手.Models
     {
         [Key]
         public int Id { get; set; } // 独立主键
+[MaxLength(50)] // 👈 告诉 EF 这个字符串最多 50 个字符，别搞成无限长
         public string Username { get; set; } = string.Empty; // 区分不同用户的代号
+[MaxLength(20)] // 👈 告诉 EF 这个字符串最多 50 个字符，别搞成无限长
         public string FundCode { get; set; } = string.Empty;
         public string FundName { get; set; } = string.Empty;
         public double HoldAmount { get; set; } // 持仓本金
