3058394976f4842b4c179f734d0305be9e348324
小白 <78636314+xiaobai5211314@users.noreply.github.com>
2026-04-30 01:49:00 +0800

    更新 User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -4,9 +4,9 @@ namespace 估值助手.Models
 {
     public class User
     {
-
         [Key]
-        public string Username { get; set; } = string.Empty; //  账号
+        public string Username { get; set; } = string.Empty; // 账号
         public string PasswordHash { get; set; } = string.Empty; // 加密后的密码
+        public string AvatarDataUrl { get; set; } = string.Empty; // 用户头像 DataURL，存数据库
     }
 }
