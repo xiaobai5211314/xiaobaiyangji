8fa1a98c8b7b8c8da2365a6bb229ff170604a27f
xiaobai <xiaobai521@88.com>
2026-05-03 16:31:44 +0800

    差异化文件

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -8,6 +8,10 @@ namespace 估值助手.Models
         public DbSet<MyFundConfig> MyFunds { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<DailyArchive> DailyArchives { get; set; }
+        public DbSet<FundValuationEstimate> FundValuationEstimates { get; set; }
+        public DbSet<FundValuationCalibration> FundValuationCalibrations { get; set; }
+        public DbSet<UserUiState> UserUiStates { get; set; }
+        public DbSet<UserInsightSnapshot> UserInsightSnapshots { get; set; }
         public DbSet<OcrCorrection> OcrCorrections { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
@@ -83,6 +87,33 @@ namespace 估值助手.Models
             modelBuilder.Entity<OcrCorrection>()
                 .Property(x => x.FundName)
                 .IsRequired();
+            modelBuilder.Entity<FundValuationEstimate>()
+    .HasIndex(x => new { x.Username, x.FundCode, x.TradeDate })
+    .IsUnique()
+    .HasDatabaseName("IX_FundValuationEstimate_User_Code_Date");
+
+            modelBuilder.Entity<FundValuationEstimate>()
+                .HasIndex(x => new { x.Username, x.TradeDate })
+                .HasDatabaseName("IX_FundValuationEstimate_User_Date");
+
+            modelBuilder.Entity<FundValuationCalibration>()
+                .HasIndex(x => new { x.Username, x.FundCode, x.TradeDate })
+                .IsUnique()
+                .HasDatabaseName("IX_FundValuationCalibration_User_Code_Date");
+
+            modelBuilder.Entity<FundValuationCalibration>()
+                .HasIndex(x => new { x.Username, x.FundCode })
+                .HasDatabaseName("IX_FundValuationCalibration_User_Code");
+
+            modelBuilder.Entity<UserUiState>()
+                .HasIndex(x => new { x.Username, x.StateKey })
+                .IsUnique()
+                .HasDatabaseName("IX_UserUiState_User_Key");
+
+            modelBuilder.Entity<UserInsightSnapshot>()
+                .HasIndex(x => new { x.Username, x.SnapshotType, x.SnapshotDate })
+                .IsUnique()
+                .HasDatabaseName("IX_UserInsightSnapshot_User_Type_Date");
         }
     }
 }
